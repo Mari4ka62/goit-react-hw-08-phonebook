74436d6054d6b17df5c3fb3c689f1adfd4c27c52
Mari4ka62 <92303981+Mari4ka62@users.noreply.github.com>
2023-06-29 22:01:25 +0000

    Deploying to gh-pages from @ Mari4ka62/goit-react-hw-08-phonebook@4eb9a8e4cb570e73d05a743391a39e3f9a84783a 🚀

---
+++ static/js/415.0047c58a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,t,r){r.r(t);r(791);var c=r(184);t.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("h1",{children:"Welcome to your contact book"})})}}}]);
+//# sourceMappingURL=415.0047c58a.chunk.js.map
